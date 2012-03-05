4ab1b90c955ec9d472ff807018dba48168b75d34
Dimitar Peychinov <dp86jd@gmail.com>
2012-03-05 13:29:25 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RuPIM::Application.config.secret_token = 'bba40f39954f74af43e4a6455b323802e6c05c9f493f4fbe1377d8ff08ea8bd30986f2a711462cd8112f128ef3ac1ba39cc3f28fbf7303a0475635111a3d10a3'
